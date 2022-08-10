17a3870e335580d8cd2ded1d515c29d0cd65846f
毕夏 <978260577@qq.com>
2022-08-10 21:59:20 +0800

    Fig4: Change the belief aggregation assumptions; enable several types of decision-making rules

---
+++ Consensus/Fig4/Superior.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+# @Time     : 7/19/2022 19:05
+# @Author   : Junyi
+# @FileName: Superior.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
+import math
+from Individual import Individual
+import numpy as np
+from Reality import Reality
+
+
+class Superior:
+    def __init__(self, m=None, s=None, t=None, n=None, reality=None, confirm=True):
+        self.m = m  # state length
+        self.s = s  # lower-level interdependency
+        self.t = t  # upper-level interdependency
+        self.n = n  # the number of subunits under this superior
+        self.policy_num = self.m // self.s
+        self.policy = np.random.choice([-1, 1], self.policy_num, p=[0.5, 0.5])
+        self.individuals = []
+        self.beliefs = []
+        for i in range(self.n):
+            individual = Individual(m=self.m, s=self.s, t=self.t, reality=reality)
+            if confirm:
+                individual.confirm_to_supervision(policy=self.policy)
+            self.individuals.append(individual)
+            self.beliefs.append(individual.belief)
+        self.reality = reality
+        self.payoff = self.reality.get_policy_payoff(policy=self.policy)
+
+    def local_search(self):
+        """
+        Superior can do a free local search
+        """
+        focal_index = np.random.randint(0, self.policy_num)
+        next_policy = self.policy.copy()
+        if next_policy[focal_index] == 0:
+            next_policy[focal_index] = np.random.choice([-1, 1])
+        else:
+            next_policy[focal_index] *= -1
+        next_payoff = self.reality.get_policy_payoff(policy=next_policy)
+        if next_payoff > self.payoff:
+            self.policy = next_policy
+            self.payoff = next_payoff
+            for individual in self.individuals:
+                individual.constrained_local_search(focal_policy=self.policy[focal_index], focal_policy_index=focal_index)
+
+    def get_diversity(self):
+        belief_pool = [individual.belief for individual in self.individuals]
+        diversity = 0
+        for individual in self.individuals:
+            one_pair_diversity = [self.get_distance(individual.belief, belief_b) for belief_b in belief_pool]
+            diversity += sum(one_pair_diversity) / self.m
+        return diversity / self.n
+
+    def get_distance(self, a=None, b=None):
+        acc = 0
+        for i in range(self.m):
+            if a[i] == b[i]:
+                acc += 1
+        return acc
+
+
+if __name__ == '__main__':
+    m = 27
+    s = 1
+    t = 3
+    n = 4
+    alpha = 0.5
+    reality = Reality(m=m, s=s, t=t)
+    superior = Superior(m=m, s=s, t=t, n=n, reality=reality, confirm=True)
+    for _ in range(100):
+        superior.local_search()
+        print(superior.payoff)
+        # print("*"*10)
+    # superior.describe()
+    # truth_payoff = reality.get_policy_payoff(policy=reality.real_policy)
+    # truth_payoff = reality.get_policy_payoff(policy=reality.real_policy)
+    # print("The truth payoff is: ", truth_payoff)
---
+++ Consensus/Fig4/hs.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+# @Time     : 8/5/2022 20:22
+# @Author   : Junyi
+# @FileName: Exp_s.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
+from Superior import Superior
+from Reality import Reality
+# import matplotlib
+# matplotlib.use('agg')
+# import matplotlib.pyplot as plt
+import pickle
+import time
+
+
+t0 = time.time()
+m = 120  # Christina's paper: 100
+s_list = [1, 3, 5, 7, 9, 11]
+t = 2
+n = 500  # Christina's paper: 280
+search_round = 500
+repetition_round = 200  # Christina's paper
+d_across_para = []
+h_across_para = []
+
+for s in s_list:  # parameter
+    if m % (s * t) != 0:
+        m = s * t * (m // s // t)  # deal with the cell number issue
+    manager_payoff_across_repeat = []
+    for _ in range(repetition_round):  # repetation
+        reality = Reality(m=m, s=s, t=t)
+        superior = Superior(m=m, s=s, t=t, n=n, reality=reality, confirm=True)
+        manager_payoff_across_time = []
+        for _ in range(search_round):  # free search loop
+            superior.local_search()
+            manager_performance = [individual.payoff for individual in superior.individuals]
+            manager_payoff_across_time.append(sum(manager_performance) / len(manager_performance))
+        manager_payoff_across_repeat.append(manager_payoff_across_time)
+    result_1 = []
+    for index in range(search_round):
+        temp = [payoff_list[index] for payoff_list in manager_payoff_across_repeat]
+        result_1.append(sum(temp) / len(temp))
+    h_across_para.append(result_1)
+
+# Save the original data for further analysis
+with open("Hierarchy_performance_s", 'wb') as out_file:
+    pickle.dump(h_across_para, out_file)
+t1 = time.time()
+print(t1 - t0)
+
+# x = range(search_round)
+# plt.plot(x, overall_across_para[0], "k-", label="s=1")
+# plt.plot(x, overall_across_para[1], "k--", label="s=3")
+# plt.plot(x, overall_across_para[2], "k:", label="s=5")
+# plt.title('Overall Performance')
+# plt.xlabel('Time')
+# plt.ylabel('Performance')
+# plt.legend()
+# plt.savefig("Hierarchy_s_overall_performance.jpg")
+# plt.clf()
+#
+# # Only managers
+# x = range(search_round)
+# plt.plot(x, manager_across_para[0], "k-", label="s=1")
+# plt.plot(x, manager_across_para[1], "k--", label="s=3")
+# plt.plot(x, manager_across_para[2], "k:", label="s=5")
+# plt.title('Manager Performance')
+# plt.xlabel('Time')
+# plt.ylabel('Performance')
+# plt.legend()
+# plt.savefig("Hierarchy_s_manager_performance.jpg")
+# plt.clf()
+#
+# # Only superior
+# x = range(search_round)
+# plt.plot(x, superior_across_para[0], "k-", label="s=1")
+# plt.plot(x, superior_across_para[1], "k--", label="s=3")
+# plt.plot(x, superior_across_para[2], "k:", label="s=5")
+# plt.title('Superior Performance')
+# plt.xlabel('Time')
+# plt.ylabel('Performance')
+# plt.legend()
+# plt.savefig("Hierarchy_s_superior_performance.jpg")
+# plt.clf()
+#
+# x = range(search_round)
+# plt.plot(x, pure_superior_across_para[0], "k-", label="s=1")
+# plt.plot(x, pure_superior_across_para[1], "k--", label="s=3")
+# plt.plot(x, pure_superior_across_para[2], "k:", label="s=5")
+# plt.title('Pure Superior Performance')
+# plt.xlabel('Time')
+# plt.ylabel('Performance')
+# plt.legend()
+# plt.savefig("Hierarchy_s_pure_superior_performance.jpg")
+# plt.clf()
+# plt.close()
