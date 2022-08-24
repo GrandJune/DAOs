cbe5bd99f4055b9301ab5e9d09d86e79a5fd6da7
毕夏 <978260577@qq.com>
2022-08-24 17:18:58 +0800

    Fix exps

--- Consensus/Fig1/ds.py
+++ Consensus/Fig1/ds.py
@@ -14,63 +14,112 @@ import time
 import numpy as np
 import multiprocessing as mp
 
+#
+# def func(m=None, s=None, t=None, authority=None, n=None, search_round=None,
+#          version="Rushed", loop=None, return_dict=None):
+#     reality = Reality(m=m, s=s, t=t, version=version)
+#     superior = Superior(m=m, s=s, t=t, n=n, reality=reality, authority=authority)
+#     consensus = [0] * (m // s)
+#     performance_across_time = []
+#     for _ in range(search_round):
+#         for individual in superior.individuals:
+#             next_index = np.random.choice(len(consensus))
+#             next_policy = consensus[next_index]
+#             individual.constrained_local_search_under_consensus(focal_policy=next_policy, focal_policy_index=next_index)
+#         consensus = []
+#         for i in range(m//s):
+#             temp = sum(individual.policy[i] for individual in superior.individuals)
+#             if temp < 0:
+#                 consensus.append(-1)
+#             elif temp > 0:
+#                 consensus.append(1)
+#             else:
+#                 consensus.append(0)
+#         performance_list = [individual.payoff for individual in superior.individuals]
+#         performance_across_time.append(sum(performance_list) / len(performance_list))
+#     return_dict[loop] = performance_across_time
+#
+#
+# if __name__ == '__main__':
+#     t0 = time.time()
+#     m = 60
+#     s_list = [1, 2, 3, 4, 5, 6]
+#     t = 1
+#     n = 200
+#     search_round = 600
+#     repetition_round = 400
+#     version = "Rushed"
+#     authority = False  # !!!!!!!!!!!!!!!! Without authority !!!!!!!!!!!!!!!!!!
+#     data_across_para = []
+#     for s in s_list:
+#         manager = mp.Manager()
+#         return_dict = manager.dict()
+#         jobs = []
+#         for loop in range(repetition_round):
+#             p = mp.Process(target=func, args=(m, s, t, authority, n, search_round, version, loop, return_dict))
+#             jobs.append(p)
+#             p.start()
+#
+#         for proc in jobs:
+#             proc.join()
+#         performance_across_repetition = return_dict.values()
+#
+#         result_1 = []
+#         for i in range(search_round):
+#             temp = [payoff_list[i] for payoff_list in performance_across_repetition]
+#             result_1.append(sum(temp) / len(temp))
+#         data_across_para.append(result_1)
+#     # Save the original data for further analysis
+#     with open("dao_performance_across_s", 'wb') as out_file:
+#         pickle.dump(data_across_para, out_file)
+#     t1 = time.time()
+#     print(time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
 
-def func(m=None, s=None, t=None, authority=None, n=None, search_round=None,
-         version="Rushed", loop=None, return_dict=None):
-    reality = Reality(m=m, s=s, t=t, version=version)
-    superior = Superior(m=m, s=s, t=t, n=n, reality=reality, authority=authority)
-    consensus = [0] * (m // s)
-    performance_across_time = []
-    for _ in range(search_round):
-        for individual in superior.individuals:
-            next_index = np.random.choice(len(consensus))
-            next_policy = consensus[next_index]
-            individual.constrained_local_search_under_consensus(focal_policy=next_policy, focal_policy_index=next_index)
-        consensus = []
-        for i in range(m//s):
-            temp = sum(individual.policy[i] for individual in superior.individuals)
-            if temp < 0:
-                consensus.append(-1)
-            elif temp > 0:
-                consensus.append(1)
-            else:
-                consensus.append(0)
-        performance_list = [individual.payoff for individual in superior.individuals]
-        performance_across_time.append(sum(performance_list) / len(performance_list))
-    return_dict[loop] = performance_across_time
 
+# Single process
 
-if __name__ == '__main__':
-    t0 = time.time()
-    m = 60
-    s_list = [1, 2, 3, 4, 5, 6]
-    t = 1
-    n = 200
-    search_round = 600
-    repetition_round = 400
-    version = "Rushed"
-    authority = False  # !!!!!!!!!!!!!!!! Without authority !!!!!!!!!!!!!!!!!!
-    data_across_para = []
-    for s in s_list:
-        manager = mp.Manager()
-        return_dict = manager.dict()
-        jobs = []
-        for loop in range(repetition_round):
-            p = mp.Process(target=func, args=(m, s, t, authority, n, search_round, version, loop, return_dict))
-            jobs.append(p)
-            p.start()
+t0 = time.time()
+m = 60
+s_list = [1, 2, 3, 4, 5, 6]
+t = 1
+n = 500
+search_round = 800
+repetition_round = 600
+version = "Rushed"
+authority = False  # !!!!!!!!!!!!!!!! Without authority !!!!!!!!!!!!!!!!!!
+data_across_para = []
+for s in s_list:
+    performance_across_repetition = []
+    for _ in range(repetition_round):
+        reality = Reality(m=m, s=s, t=t, version=version)
+        superior = Superior(m=m, s=s, t=t, n=n, reality=reality, authority=authority)
+        consensus = [0] * (m // s)
+        performance_across_time = []
+        for _ in range(search_round):
+            for individual in superior.individuals:
+                next_index = np.random.choice(len(consensus))
+                next_policy = consensus[next_index]
+                individual.constrained_local_search_under_consensus(focal_policy=next_policy, focal_policy_index=next_index)
+            consensus = []
+            for i in range(m//s):
+                temp = sum(individual.policy[i] for individual in superior.individuals)
+                if temp < 0:
+                    consensus.append(-1)
+                elif temp > 0:
+                    consensus.append(1)
+                else:
+                    consensus.append(0)
+            performance_list = [individual.payoff for individual in superior.individuals]
+            performance_across_time.append(sum(performance_list) / len(performance_list))
+        performance_across_repetition.append(performance_across_time)
 
-        for proc in jobs:
-            proc.join()
-        performance_across_repetition = return_dict.values()
-
-        result_1 = []
-        for i in range(search_round):
-            temp = [payoff_list[i] for payoff_list in performance_across_repetition]
-            result_1.append(sum(temp) / len(temp))
-        data_across_para.append(result_1)
-    # Save the original data for further analysis
-    with open("dao_performance_across_s", 'wb') as out_file:
-        pickle.dump(data_across_para, out_file)
-    t1 = time.time()
-    print(time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
+    result_1 = []
+    for i in range(search_round):
+        temp = [payoff_list[i] for payoff_list in performance_across_repetition]
+        result_1.append(sum(temp) / len(temp))
+    data_across_para.append(result_1)
+# Save the original data for further analysis
+with open("dao_performance_across_s", 'wb') as out_file:
+    pickle.dump(data_across_para, out_file)
+t1 = time.time()
+print(time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
--- Consensus/Fig1/evaluator.py
+++ Consensus/Fig1/evaluator.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import pickle
 
 
-folder = r"C:\Python_Workplace\dao-0820\Consensus\Fig1"
+folder = r"C:\Python_Workplace\dao-0824\Fig1"
 file_name_dao = folder + r"\DAO_performance_across_s"
 with open(file_name_dao, 'rb') as in_file:
     data_dao = pickle.load(in_file)
@@ -21,26 +21,29 @@ file_name_hierarchy = folder + r"\Hierarchy_performance_across_s"
 with open(file_name_hierarchy, 'rb') as in_file:
     data_hierarchy = pickle.load(in_file)
 
-# data_dao = [each[-1] for each in data_dao]
-# data_autonomy = [each[-1] for each in data_autonomy]
-# data_hierarchy = [each[-1] for each in data_hierarchy]
-#
-# x = range(len(data_dao))
-#
-# plt.plot(x, data_dao, "k-", label="DAO")
-# plt.plot(x, data_autonomy, "k--", label="Autonomy")
-# plt.plot(x, data_hierarchy, "k:", label="Hierarchy")
-# plt.xlabel('Operational Complexity', fontweight='bold', fontsize=10)
-# plt.ylabel('Performance', fontweight='bold', fontsize=10)
-# plt.legend(frameon=False, ncol=1)
-# plt.savefig("HAD_across_s.png", transparent=True, dpi=1200)
-# plt.savefig(folder + r"\HAD_across_s.png", transparent=True, dpi=1200)
-# plt.show()
+
+# Only the converged value
+data_dao = [each[-1] for each in data_dao]
+data_autonomy = [each[-1] for each in data_autonomy]
+data_hierarchy = [each[-1] for each in data_hierarchy]
+
+x = range(1, len(data_dao)+1)
+
+plt.plot(x, data_dao, "k-", label="DAO")
+plt.plot(x, data_autonomy, "k--", label="Autonomy")
+plt.plot(x, data_hierarchy, "k:", label="Hierarchy")
+plt.xlabel('Operational Complexity', fontweight='bold', fontsize=10)
+plt.ylabel('Performance', fontweight='bold', fontsize=10)
+plt.legend(frameon=False, ncol=1)
+plt.savefig("HAD_across_s.png", transparent=True, dpi=1200)
+plt.savefig(folder + r"\HAD_across_s.png", transparent=True, dpi=1200)
+plt.show()
 
 # Test the round
-# x = range(len(data_hierarchy[0]))
+# x = range(len(data_dao[0]))
 #
-# plt.plot(x, data_hierarchy[-3], "k-", label="s1")
-# plt.plot(x, data_hierarchy[-2], "k--", label="s2")
-# plt.plot(x, data_hierarchy[-1], "k:", label="s2")
+# plt.plot(x, data_dao[0], "k-", label="s1")
+# plt.plot(x, data_dao[1], "k--", label="s2")
+# plt.plot(x, data_dao[2], "k:", label="s3")
+# plt.legend(frameon=False, ncol=1)
 # plt.show()
