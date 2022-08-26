054940283ea174f53f1b01a6da5eccd8f3090465
毕夏 <978260577@qq.com>
2022-08-26 21:55:43 +0800

    Fix exps

--- Consensus/Fig2_limited_process/autonomy.py
+++ Consensus/Fig2_limited_process/autonomy.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Time     : 8/5/2022 20:22
 # @Author   : Junyi
-# @FileName: Exp_s.py
+# @FileName: as.py
 # @Software  : PyCharm
 # Observing PEP 8 coding style
 from Superior import Superior
@@ -11,32 +11,19 @@ from Reality import Reality
 # import matplotlib.pyplot as plt
 import pickle
 import time
-import numpy as np
 import multiprocessing as mp
 from multiprocessing import Pool
 
-#
+# mp version
 # def func(m=None, s=None, t=None, authority=None, n=None, search_round=None,
 #          version="Rushed", loop=None, return_dict=None):
 #     reality = Reality(m=m, s=s, t=t, version=version)
 #     superior = Superior(m=m, s=s, t=t, n=n, reality=reality, authority=authority)
-#     consensus = [0] * (m // s)
 #     diversity_across_time = []
-#     for _ in range(search_round):
+#     for _ in range(search_round):  # free search loop
+#         diversity_across_time.append(superior.get_diversity())
 #         for individual in superior.individuals:
-#             diversity_across_time.append(superior.get_diversity())
-#             next_index = np.random.choice(len(consensus))
-#             next_policy = consensus[next_index]
-#             individual.constrained_local_search_under_consensus(focal_policy=next_policy, focal_policy_index=next_index)
-#         consensus = []
-#         for i in range(m//s):
-#             temp = sum(individual.policy[i] for individual in superior.individuals)
-#             if temp < 0:
-#                 consensus.append(-1)
-#             elif temp > 0:
-#                 consensus.append(1)
-#             else:
-#                 consensus.append(0)
+#             individual.free_local_search()
 #     return_dict[loop] = diversity_across_time
 #
 #
@@ -64,7 +51,7 @@ from multiprocessing import Pool
 #
 #         for proc in jobs:
 #             proc.join()
-#         diversity_across_repetition = return_dict.values()
+#         diversity_across_repetition = return_dict.values()  # Don't need dict index, since it is repetition.
 #
 #         result_1 = []
 #         for i in range(search_round):
@@ -72,14 +59,14 @@ from multiprocessing import Pool
 #             result_1.append(sum(temp) / len(temp))
 #         data_across_para.append(result_1)
 #     # Save the original data for further analysis
-#     with open("dao_performance_across_s", 'wb') as out_file:
+#     with open("Autonomy_diversity_across_s", 'wb') as out_file:
 #         pickle.dump(data_across_para, out_file)
 #     t1 = time.time()
 #     print(time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
 
 
 
-# 单线程
+# Single process
 # t0 = time.time()
 # m = 60
 # s_list = [2, 3, 4, 5, 6]
@@ -91,65 +78,42 @@ from multiprocessing import Pool
 # authority = False  # !!!!!!!!!!!!!!!! Without authority !!!!!!!!!!!!!!!!!!
 # data_across_para = []
 # for s in s_list:
+#     m = 60
+#     m = s * t * (m // (s * t))
 #     diversity_across_repetition = []
 #     for _ in range(repetition_round):
 #         reality = Reality(m=m, s=s, t=t, version=version)
 #         superior = Superior(m=m, s=s, t=t, n=n, reality=reality, authority=authority)
-#         consensus = [0] * (m // s)
 #         diversity_across_time = []
-#         for _ in range(search_round):
+#         for _ in range(search_round):  # free search loop
+#             diversity_across_time.append(superior.get_diversity())
 #             for individual in superior.individuals:
-#                 diversity_across_time.append(superior.get_diversity())
-#                 next_index = np.random.choice(len(consensus))
-#                 next_policy = consensus[next_index]
-#                 individual.constrained_local_search_under_consensus(focal_policy=next_policy, focal_policy_index=next_index)
-#             consensus = []
-#             for i in range(m//s):
-#                 temp = sum(individual.policy[i] for individual in superior.individuals)
-#                 if temp < 0:
-#                     consensus.append(-1)
-#                 elif temp > 0:
-#                     consensus.append(1)
-#                 else:
-#                     consensus.append(0)
+#                 individual.free_local_search()
 #         diversity_across_repetition.append(diversity_across_time)
+#
 #     result_1 = []
 #     for i in range(search_round):
 #         temp = [diversity_list[i] for diversity_list in diversity_across_repetition]
 #         result_1.append(sum(temp) / len(temp))
 #     data_across_para.append(result_1)
-#
 # # Save the original data for further analysis
-# with open("dao_performance_across_s", 'wb') as out_file:
+# with open("Autonomy_diversity_across_s", 'wb') as out_file:
 #     pickle.dump(data_across_para, out_file)
 # t1 = time.time()
 # print(time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
 
 
-
 # 有限线程
 def func(m=None, s=None, t=None, authority=None, n=None, search_round=None,
-         version="Rushed", loop=None, return_dict=None):
+         version="Rushed"):
     reality = Reality(m=m, s=s, t=t, version=version)
     superior = Superior(m=m, s=s, t=t, n=n, reality=reality, authority=authority)
-    consensus = [0] * (m // s)
     diversity_across_time = []
     for _ in range(search_round):
+        diversity_across_time.append(superior.get_diversity())
         for individual in superior.individuals:
-            diversity_across_time.append(superior.get_diversity())
-            next_index = np.random.choice(len(consensus))
-            next_policy = consensus[next_index]
-            individual.constrained_local_search_under_consensus(focal_policy=next_policy, focal_policy_index=next_index)
-        consensus = []
-        for i in range(m//s):
-            temp = sum(individual.policy[i] for individual in superior.individuals)
-            if temp < 0:
-                consensus.append(-1)
-            elif temp > 0:
-                consensus.append(1)
-            else:
-                consensus.append(0)
-    return_dict[loop] = diversity_across_time
+            individual.free_local_search()
+    return diversity_across_time
 
 
 if __name__ == '__main__':
@@ -168,18 +132,16 @@ if __name__ == '__main__':
         m = s * t * (m // (s * t))
         cpu_num = mp.cpu_count()
         pool = Pool(cpu_num)
-        result_list = []
+        diversity_across_repetition = []
         for i in range(repetition_round):
-            result_list.append(pool.apply_async(func=func, args=((m, s, t, authority, n, search_round, version))).get())
-            print("********")
-
+            diversity_across_repetition.append(pool.apply_async(func=func, args=((m, s, t, authority, n, search_round, version))).get())
         result_1 = []
         for i in range(search_round):
             temp = [diversity_list[i] for diversity_list in diversity_across_repetition]
             result_1.append(sum(temp) / len(temp))
         data_across_para.append(result_1)
     # Save the original data for further analysis
-    with open("dao_performance_across_s", 'wb') as out_file:
+    with open("Autonomy_diversity_s", 'wb') as out_file:
         pickle.dump(data_across_para, out_file)
     t1 = time.time()
     print(time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
