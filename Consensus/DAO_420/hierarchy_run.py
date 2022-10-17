628c7272366ea63933228b036108491a51cb223f
毕夏 <978260577@qq.com>
2022-10-17 16:01:16 +0800

    Set down the first experiment

--- Consensus/DAO_420/hierarchy_run.py
+++ Consensus/DAO_420/hierarchy_run.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# @Time     : 10/9/2022 22:52
+# @Time     : 10/13/2022 15:20
 # @Author   : Junyi
-# @FileName: dao_run.py
+# @FileName: hierarchy_run.py
 # @Software  : PyCharm
 # Observing PEP 8 coding style
 import numpy as np
@@ -17,25 +17,23 @@ import pickle
 import math
 
 
-def func(m=None, s=None, n=None, group_size=None, lr=None, threshold_ratio=None,
-         search_loop=None, loop=None, return_dict=None, sema=None):
+def func(m=None, s=None, n=None, group_size=None, lr=None, search_loop=None, loop=None, return_dict=None, sema=None):
     reality = Reality(m=m, s=s)
-    dao = DAO(m=m, s=s, n=n, reality=reality, lr=lr, subgroup_size=group_size)
+    hierarchy = Hierarchy(m=m, s=s, n=n, reality=reality, lr=lr, subgroup_size=group_size)
     for _ in range(search_loop):
-        dao.search(threshold_ratio=threshold_ratio)
-    return_dict[loop] = [dao.performance_across_time, dao.consensus_performance_across_time, dao.diversity_across_time]
+        hierarchy.search()
+    return_dict[loop] = [hierarchy.performance_across_time, hierarchy.superior.performance_across_time, hierarchy.diversity_across_time]
     sema.release()
-
 
 
 if __name__ == '__main__':
     t0 = time.time()
     m = 90
     s = 1
-    n = 1400
+    n = 420
     lr = 0.3
     threshold_ratio = 0.1
-    repetition = 200
+    repetition = 100
     search_loop = 1000
     group_size = 7  # the smallest group size in Fang's model: 7
     concurrency = 30
@@ -45,32 +43,36 @@ if __name__ == '__main__':
     jobs = []
     for loop in range(repetition):
         sema.acquire()
-        p = mp.Process(target=func, args=(m, s, n, group_size, lr, threshold_ratio, search_loop, loop, return_dict, sema))
+        p = mp.Process(target=func, args=(m, s, n, group_size, lr, search_loop, loop, return_dict, sema))
         jobs.append(p)
         p.start()
     for proc in jobs:
         proc.join()
     results = return_dict.values()  # Don't need dict index, since it is repetition.
     performance_across_time = [result[0] for result in results]
-    consensus_performance_across_time = [result[1] for result in results]
+    superior_performance_across_time = [result[1] for result in results]
     diversity_across_time = [result[2] for result in results]
 
     performance_across_time_final = []
-    consensus_performance_across_time_final = []
+    superior_performance_across_time_final = []
     diversity_across_time_final = []
     for index in range(search_loop):
         temp_performance = sum([result[index] for result in performance_across_time]) / search_loop
-        temp_consensus = sum([result[index] for result in consensus_performance_across_time]) / search_loop
-        temp_diversity = sum([result[index] for result in diversity_across_time]) / search_loop
+        temp_superior = sum([result[index] for result in superior_performance_across_time]) / search_loop
+        temp_diveristy = sum([result[index] for result in diversity_across_time]) / search_loop
         performance_across_time_final.append(temp_performance)
-        consensus_performance_across_time_final.append(temp_consensus)
-        diversity_across_time_final.append(temp_diversity)
+        superior_performance_across_time_final.append(temp_superior)
+        diversity_across_time_final.append(temp_diveristy)
 
-    with open("dao_performance_across_time", 'wb') as out_file:
+    with open("hierarchy_performance_across_time", 'wb') as out_file:
         pickle.dump(performance_across_time_final, out_file)
-    with open("dao_consensus_performance_across_time", 'wb') as out_file:
-        pickle.dump(consensus_performance_across_time_final, out_file)
-    with open("dao_diversity_across_time", 'wb') as out_file:
+    with open("hierarchy_superior_performance_across_time", 'wb') as out_file:
+        pickle.dump(superior_performance_across_time_final, out_file)
+    with open("hierarchy_diversity_across_time", 'wb') as out_file:
         pickle.dump(diversity_across_time_final, out_file)
     t1 = time.time()
     print(time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
+
+
+
+
