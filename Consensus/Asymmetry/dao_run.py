c3f0b0345c79e384dd91cbdb2795b989fb1bbd2b
毕夏 <978260577@qq.com>
2022-11-02 19:15:30 +0800

    fix the bug of initialization of individual beliefs (-1, 0, 1) initialization; previously (-1, 1) initialization;
    fix the bug of with-token voting; forgot to indicate the index of each policy bit

--- Consensus/Asymmetry/dao_run.py
+++ Consensus/Asymmetry/dao_run.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     repetition = 100
     search_loop = 2000
     group_size = 7  # the smallest group size in Fang's model: 7
-    concurrency = 25
+    concurrency = 50
     asymmetry_list = [0, 1, 2, 4, 8]  # smaller asymmetry is associated with higher wealth inequality
     # after taking an average across repetitions
     performance_across_para = []
