061dfacf148ffdde147957982d7ee722e387c739
毕夏 <978260577@qq.com>
2023-02-24 16:51:09 +0800

    Test the rare event of learning --Previously we test learning from zero (consensus or supervision)
    In this robustness, we test the non-learning of best peers. No learn for best actor -> I think this is impled in previous version, because in the learm_from_belief function, agents will not learn from zero. (agents never forget)
    
    Another test is, test whether the inverted U-shaped only occurs when agents can learn from chaos (when there is no consensus yet); In the Threshold_stop_learn version, recall the stop learn constrain, to see whether the U-shape will disappear.

---
+++ V4-2/Turbulence_stop_learn/Team.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2/1/2023 10:48
+# @Author   : Junyi
+# @FileName: Team.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
+import math
+from Individual import Individual
+from Reality import Reality
+import numpy as np
+
+class Team:
+    def __init__(self, m=None, index=None, alpha=None, reality=None):
+        self.index = index
+        self.m = m
+        self.alpha = alpha
+        self.policy_num = self.m // self.alpha
+        self.individuals = []
+        self.manager = None
+        self.reality = reality
+        self.policy = [0] * self.policy_num
+        self.belief = [0] * self.m
+        self.token = None
+
+    def form_individual_majority_view(self):
+        for individual in self.individuals:
+            superior_belief_pool = [other.belief for other in self.individuals
+                                    if other.payoff > individual.payoff]
+            majority_view = []
+            # In this robustness, we explicitly constrain this boundary of superior majority view learning
+            # In previous code, it is implied in the learn_from_belief function
+            # where agents never learn from zero, namely never forget.
+            if len(superior_belief_pool) == 0:
+                individual.superior_majority_view = None
+            else:
+                for i in range(self.m):
+                    temp = [belief[i] for belief in superior_belief_pool]
+                    if sum(temp) > 0:
+                        majority_view.append(1)
+                    elif sum(temp) < 0:
+                        majority_view.append(-1)
+                    else:  # when there is no inclination as reference, agents will become uncertain
+                        majority_view.append(0)
+                individual.superior_majority_view = majority_view
+
+    def adjust_majority_view_2_consensus(self, policy=None):
+        for individual in self.individuals:
+            if not individual.superior_majority_view:
+                continue
+            for index in range(self.policy_num):
+                # !!!!!!!!!!!!!!!!!!!!!! #
+                # if the consensus is zero, will not learn from consensus
+                if policy[index] == 0:
+                    continue
+                if sum(individual.superior_majority_view
+                       [index * self.alpha: (index + 1) * self.alpha]) != policy[index]:
+                    individual.superior_majority_view[index * self.alpha: (index + 1) * self.alpha] = \
+                        self.reality.policy_2_belief(policy=policy[index])
+
+    def confirm(self, policy=None):
+        # individual first confirm to the supervision
+        for individual in self.individuals:
+            for index in range(self.policy_num):
+                # if the supervision is zero, will retain the previous belief (lazy employee)
+                if policy[index] == 0:
+                    continue
+                else:
+                    if sum(individual.belief[index * self.alpha: (index + 1) * self.alpha]) != policy[index]:
+                        individual.belief[index * self.alpha: (index + 1) * self.alpha] = self.reality.policy_2_belief(
+                            policy=policy[index])
+
+    def learn(self):
+        for individual in self.individuals:
+            if not individual.superior_majority_view:
+                continue
+            individual.learning_from_belief(belief=individual.superior_majority_view)
+
+
+if __name__ == '__main__':
+    # test
+    m = 30
+    s = 1
+    n = 10
+    lr = 0.3
+    auto_lr = 0.5
+    loop = 100
+    version = "Rushed"
+    reality = Reality(m=m, s=s, version=version)
+    from DAO import Team
+    for _ in range(n):
+        individual = Individual(m=m, s=s, reality=reality, lr=lr)
+        team.individuals.append(individual)
+    for individual in team.individuals:
+        print(individual.belief, individual.payoff)
