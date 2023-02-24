5670135aeb631f74cfd27ccff4132bbdabca66a9
毕夏 <978260577@qq.com>
2023-02-24 16:21:20 +0800

    Test the rare event of learning --Previously we test learning from zero (consensus or supervision)
    In this robustness, we test the non-learning of best peers. No learn for best actor -> I think this is impled in previous version, because in the learm_from_belief function, agents will not learn from zero. (agents never forget)
    
    Another test is, test whether the inverted U-shaped only occurs when agents can learn from chaos (when there is no consensus yet); In the Threshold_stop_learn version, recall the stop learn constrain, to see whether the U-shape will disappear.

--- V4-2/Turnover/Team.py
+++ V4-2/Turnover/Team.py
@@ -27,7 +27,9 @@ class Team:
             superior_belief_pool = [other.belief for other in self.individuals
                                     if other.payoff > individual.payoff]
             majority_view = []
-            # In this robustness, we strictly constrain this boundary of superior majority view learning
+            # In this robustness, we explicitly constrain this boundary of superior majority view learning
+            # In previous code, it is implied in the learn_from_belief function
+            # where agents never learn from zero, namely never forget.
             if len(superior_belief_pool) == 0:
                 individual.superior_majority_view = None
             else:
