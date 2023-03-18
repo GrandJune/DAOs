5bbbc506df9819f984ed72eeed746fe2584170a3
毕夏 <978260577@qq.com>
2023-03-18 20:50:41 +0800

    Test the team algorithm --Best performing agents

--- V4.3/DHA/dao_run.py
+++ V4.3/DHA/dao_run.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     hyper_iteration = 10
     repetition = 50
     concurrency = 50
-    search_loop = 100
+    search_loop = 300
     group_size = 7  # the smallest group size in Fang's model: 7
     performance_hyper = []
     consensus_hyper = []
--- V4.3/DHA/hierarchy_run.py
+++ V4.3/DHA/hierarchy_run.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     hyper_iteration = 10
     repetition = 50
     concurrency = 50
-    search_loop = 100
+    search_loop = 300
     group_size = 7  # the smallest group size in Fang's model: 7
     performance_across_time_hyper = []
     superior_performance_across_time_hyper = []
