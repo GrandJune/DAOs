801d4874ad5cf13b92b62a875dfb93ccd44824a2
毕夏 <978260577@qq.com>
2023-05-25 21:41:36 +0800

    after ICIS 2023:
    --add the 3D surface for robustness across p1, p2
    --add interaction: threshold + inactive; threshold + asymmetry
    --remove parameter s

--- Interactions/Threshold_inactive/dao_run_3.py
+++ Interactions/Threshold_inactive/dao_run_3.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     repetition = 50
     search_loop = 2000
     threshold_ratio_list = np.arange(0.40, 0.71, 0.01)
-    inactive_list = [0.1]
+    inactive_list = [0.3]
     group_size = 7  # the smallest group size in Fang's model: 7
     concurrency = 50
     for inactive in inactive_list:
