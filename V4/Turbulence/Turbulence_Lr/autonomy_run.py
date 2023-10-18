45f3e3e65c5ef6cd4a85a055b93bdff07976228b
毕夏 <978260577@qq.com>
2023-10-18 16:33:27 +0800

    after ICIS 2023:
    --add the 3D surface for robustness across p1, p2
    --add interaction: threshold + inactive; threshold + asymmetry; threshold + incentive
    --remove parameter s
    --add the Simplified Version to reproduce the main results of criticality (threshold, asymmetry, incentive)
    --regularize the code norm including parameter type and default value

--- V4/Turbulence/Turbulence_Lr/autonomy_run.py
+++ V4/Turbulence/Turbulence_Lr/autonomy_run.py
@@ -17,12 +17,17 @@ import pickle
 import math
 
 
-def func(m=None, n=None, group_size=None, lr=None,
+def func(m=None, n=None, group_size=None, lr=None, turbulence_freq=None,
          search_loop=None, loop=None, return_dict=None, sema=None):
     np.random.seed(None)
     reality = Reality(m=m)
     autonomy = Autonomy(m=m, n=n, reality=reality, group_size=group_size, lr=lr)
-    for _ in range(search_loop):
+    for period in range(search_loop):
+        if (period + 1) % turbulence_freq == 0:
+            reality.change(reality_change_rate=0.15)
+            for team in autonomy.teams:
+                for individual in team.individuals:
+                    individual.payoff = reality.get_payoff(belief=individual.belief)
         autonomy.search()
     return_dict[loop] = [autonomy.performance_across_time, autonomy.diversity_across_time,
                          autonomy.variance_across_time]
@@ -32,85 +37,53 @@ def func(m=None, n=None, group_size=None, lr=None,
 if __name__ == '__main__':
     t0 = time.time()
     m = 90
+    turbulence_freq_list = [20, 40, 60, 80, 100]
+    group_size = 7
     n = 350
     lr_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
     repetition = 200
     concurrency = 50
     search_loop = 1000
-    group_size = 7  # the smallest group size in Fang's model: 7
-    # DVs
-    performance_across_para = []
-    diversity_across_para = []
-    variance_across_para = []
-
-    performance_across_para_time = []
-    diversity_across_para_time = []
-    variance_across_para_time = []
     for lr in lr_list:
-        sema = Semaphore(concurrency)
-        manager = mp.Manager()
-        return_dict = manager.dict()
-        jobs = []
-        for loop in range(repetition):
-            sema.acquire()
-            p = mp.Process(target=func,
-                           args=(m, n, group_size, lr, search_loop, loop, return_dict, sema))
-            jobs.append(p)
-            p.start()
-        for proc in jobs:
-            proc.join()
-        results = return_dict.values()  # Don't need dict index, since it is repetition.
+        for turbulence_freq in turbulence_freq_list:
+            sema = Semaphore(concurrency)
+            manager = mp.Manager()
+            return_dict = manager.dict()
+            jobs = []
+            for loop in range(repetition):
+                sema.acquire()
+                p = mp.Process(target=func,
+                               args=(m, n, group_size, lr, turbulence_freq, search_loop, loop, return_dict, sema))
+                jobs.append(p)
+                p.start()
+            for proc in jobs:
+                proc.join()
+            results = return_dict.values()  # Don't need dict index, since it is repetition.
+            # keep the time dimension
+            performance_across_repeat_time = [result[0] for result in results]
+            diversity_across_repeat_time = [result[1] for result in results]
+            variance_across_repeat_time = [result[2] for result in results]
 
-        # remove the time dimension, only keep the last value
-        performance_across_repeat = [result[0][-1] for result in results]
-        diversity_across_repeat = [result[1][-1] for result in results]
-        variance_across_repeat = [result[2][-1] for result in results]
+            performance_across_time = []
+            diversity_across_time = []
+            variance_across_time = []
+            for period in range(search_loop):
+                temp_performance = [performance_list[period] for performance_list in performance_across_repeat_time]
+                performance_across_time.append(sum(temp_performance) / len(temp_performance))
 
-        # take an average across repetition, only one value for one parameter
-        performance_across_para.append(sum(performance_across_repeat) / len(performance_across_repeat))
-        diversity_across_para.append(sum(diversity_across_repeat) / len(diversity_across_repeat))
-        variance_across_para.append(sum(variance_across_repeat) / len(variance_across_repeat))
+                temp_diversity = [diversity_list[period] for diversity_list in diversity_across_repeat_time]
+                diversity_across_time.append(sum(temp_diversity) / len(temp_diversity))
 
-        # keep the time dimension
-        performance_across_repeat_time = [result[0] for result in results]
-        diversity_across_repeat_time = [result[1] for result in results]
-        variance_across_repeat_time = [result[2] for result in results]
+                temp_variance = [variance_list[period] for variance_list in variance_across_repeat_time]
+                variance_across_time.append(sum(temp_variance) / len(temp_variance))
 
-        # take an average across repetition, for each time iteration, integrate into [loop] values for one parameter
-        performance_across_time = []  # under the same parameter
-        diversity_across_time = []
-        variance_across_time = []
-        percentile_10_across_time = []
-        percentile_90_across_time = []
-        for period in range(search_loop):
-            temp_performance = [performance_list[period] for performance_list in performance_across_repeat_time]
-            performance_across_time.append(sum(temp_performance) / len(temp_performance))
-
-            temp_diversity = [diversity_list[period] for diversity_list in diversity_across_repeat_time]
-            diversity_across_time.append(sum(temp_diversity) / len(temp_diversity))
-
-            temp_variance = [variance_list[period] for variance_list in variance_across_repeat_time]
-            variance_across_time.append(sum(temp_variance) / len(temp_variance))
-        # retain the time dimension
-        performance_across_para_time.append(performance_across_time)
-        diversity_across_para_time.append(diversity_across_time)
-        variance_across_para_time.append(variance_across_time)
-
-    # save the without-time data
-    with open("autonomy_performance_across_lr", 'wb') as out_file:
-        pickle.dump(performance_across_para, out_file)
-    with open("autonomy_diversity_across_lr", 'wb') as out_file:
-        pickle.dump(diversity_across_para, out_file)
-    with open("autonomy_variance_across_lr", 'wb') as out_file:
-        pickle.dump(variance_across_para, out_file)
-
-    # save the with-time data
-    with open("autonomy_performance_across_lr_time", 'wb') as out_file:
-        pickle.dump(performance_across_para_time, out_file)
-    with open("autonomy_diversity_across_lr_time", 'wb') as out_file:
-        pickle.dump(diversity_across_para_time, out_file)
-    with open("autonomy_variance_across_lr_time", 'wb') as out_file:
-        pickle.dump(variance_across_para_time, out_file)
+            # save the with-time data
+            with open("autonomy_performance_across_time_freq_{}_lr_{}".format(turbulence_freq, lr), 'wb') as out_file:
+                pickle.dump(performance_across_time, out_file)
+            with open("autonomy_diversity_across_time_freq_{}_lr_{}".format(turbulence_freq, lr), 'wb') as out_file:
+                pickle.dump(diversity_across_time, out_file)
+            with open("autonomy_variance_across_time_freq_{}_lr_{}".format(turbulence_freq, lr), 'wb') as out_file:
+                pickle.dump(variance_across_time, out_file)
 
     t1 = time.time()
-    print(time.strftime("%H:%M:%S", time.gmtime(t1 - t0)))
+    print("Autonomy: ", time.strftime("%H:%M:%S", time.gmtime(t1 - t0)))
--- V4/Turbulence/Turbulence_Lr/hierarchy_run.py
+++ V4/Turbulence/Turbulence_Lr/hierarchy_run.py
@@ -5,16 +5,12 @@
 # @Software  : PyCharm
 # Observing PEP 8 coding style
 import numpy as np
-from DAO import DAO
 from Hierarchy import Hierarchy
-from Autonomy import Autonomy
 from Reality import Reality
 import multiprocessing as mp
 import time
-from multiprocessing import Pool
 from multiprocessing import Semaphore
 import pickle
-import math
 
 
 def func(m=None, n=None, group_size=None, lr=None, search_loop=None, loop=None, return_dict=None, sema=None):
@@ -31,102 +27,61 @@ def func(m=None, n=None, group_size=None, lr=None, search_loop=None, loop=None, 
 if __name__ == '__main__':
     t0 = time.time()
     m = 90
+    turbulence_freq_list = [20, 40, 60, 80, 100]
+    group_size = 7
     n = 350
     lr_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
     repetition = 200
     concurrency = 50
     search_loop = 1000
-    group_size = 7  # the smallest group size in Fang's model: 7
-    # DVs
-    performance_across_para = []
-    superior_performance_across_para = []
-    diversity_across_para = []
-    variance_across_para = []
-
-    performance_across_para_time = []
-    superior_performance_across_para_time = []
-    diversity_across_para_time = []
-    variance_across_para_time = []
     for lr in lr_list:
-        sema = Semaphore(concurrency)
-        manager = mp.Manager()
-        return_dict = manager.dict()
-        jobs = []
-        for loop in range(repetition):
-            sema.acquire()
-            p = mp.Process(target=func,
-                           args=(m, n, group_size, lr, search_loop, loop, return_dict, sema))
-            jobs.append(p)
-            p.start()
-        for proc in jobs:
-            proc.join()
-        results = return_dict.values()  # Don't need dict index, since it is repetition.
+        for turbulence_freq in turbulence_freq_list:
+            sema = Semaphore(concurrency)
+            manager = mp.Manager()
+            return_dict = manager.dict()
+            jobs = []
+            for loop in range(repetition):
+                sema.acquire()
+                p = mp.Process(target=func,
+                               args=(m, n, group_size, lr, turbulence_freq, search_loop, loop, return_dict, sema))
+                jobs.append(p)
+                p.start()
+            for proc in jobs:
+                proc.join()
+            results = return_dict.values()  # Don't need dict index, since it is repetition.
+            # keep the time dimension
+            performance_across_repeat_time = [result[0] for result in results]
+            superior_performance_across_repeat_time = [result[1] for result in results]
+            diversity_across_repeat_time = [result[2] for result in results]
+            variance_across_repeat_time = [result[3] for result in results]
 
-        # remove the time dimension, only keep the last value
-        performance_across_repeat = [result[0][-1] for result in results]
-        superior_performance_across_repeat = [result[1][-1] for result in results]
-        diversity_across_repeat = [result[2][-1] for result in results]
-        variance_across_repeat = [result[3][-1] for result in results]
+            performance_across_time = []
+            superior_performance_across_time = []
+            diversity_across_time = []
+            variance_across_time = []
+            for period in range(search_loop):
+                temp_performance = [performance_list[period] for performance_list in performance_across_repeat_time]
+                performance_across_time.append(sum(temp_performance) / len(temp_performance))
 
-        # take an average across repetition, only one value for one parameter
-        performance_across_para.append(sum(performance_across_repeat) / len(performance_across_repeat))
-        superior_performance_across_para.append(
-            sum(superior_performance_across_repeat) / len(superior_performance_across_repeat))
-        diversity_across_para.append(sum(diversity_across_repeat) / len(diversity_across_repeat))
-        variance_across_para.append(sum(variance_across_repeat) / len(variance_across_repeat))
+                temp_consensus_performance = [performance_list[period] for performance_list in
+                                              superior_performance_across_repeat_time]
+                superior_performance_across_time.append(sum(temp_consensus_performance) / len(temp_consensus_performance))
 
-        # keep the time dimension
-        performance_across_repeat_time = [result[0] for result in results]
-        superior_performance_across_repeat_time = [result[1] for result in results]
-        diversity_across_repeat_time = [result[2] for result in results]
-        variance_across_repeat_time = [result[3] for result in results]
+                temp_diversity = [diversity_list[period] for diversity_list in diversity_across_repeat_time]
+                diversity_across_time.append(sum(temp_diversity) / len(temp_diversity))
 
-        # take an average across repetition, for each time iteration, integrate into 600 values for one parameter
-        performance_across_time = []  # under the same parameter
-        superior_performance_across_time = []
-        diversity_across_time = []
-        variance_across_time = []
-        for period in range(search_loop):
-            temp_performance = [performance_list[period] for performance_list in performance_across_repeat_time]
-            performance_across_time.append(sum(temp_performance) / len(temp_performance))
-            temp_superior_performance = [performance_list[period] for performance_list in
-                                         superior_performance_across_repeat_time]
-            superior_performance_across_time.append(sum(temp_superior_performance) / len(temp_superior_performance))
+                temp_variance = [variance_list[period] for variance_list in variance_across_repeat_time]
+                variance_across_time.append(sum(temp_variance) / len(temp_variance))
 
-            temp_diversity = [diversity_list[period] for diversity_list in diversity_across_repeat_time]
-            diversity_across_time.append(sum(temp_diversity) / len(temp_diversity))
-
-            temp_variance = [variance_list[period] for variance_list in variance_across_repeat_time]
-            variance_across_time.append(sum(temp_variance) / len(temp_variance))
-        # retain the time dimension
-        performance_across_para_time.append(performance_across_time)
-        superior_performance_across_para_time.append(superior_performance_across_time)
-        diversity_across_para_time.append(diversity_across_time)
-        variance_across_para_time.append(variance_across_time)
-
-    # save the without-time data
-    with open("hierarchy_performance_across_lr", 'wb') as out_file:
-        pickle.dump(performance_across_para, out_file)
-    with open("superior_performance_across_lr", 'wb') as out_file:
-        pickle.dump(superior_performance_across_para, out_file)
-    with open("hierarchy_diversity_across_lr", 'wb') as out_file:
-        pickle.dump(diversity_across_para, out_file)
-    with open("hierarchy_variance_across_lr", 'wb') as out_file:
-        pickle.dump(variance_across_para, out_file)
-
-    # save the with-time data
-    with open("hierarchy_performance_across_lr_time", 'wb') as out_file:
-        pickle.dump(performance_across_para_time, out_file)
-    with open("superior_performance_across_lr_time", 'wb') as out_file:
-        pickle.dump(superior_performance_across_para_time, out_file)
-    with open("hierarchy_diversity_across_lr_time", 'wb') as out_file:
-        pickle.dump(diversity_across_para_time, out_file)
-    with open("hierarchy_variance_across_lr_time", 'wb') as out_file:
-        pickle.dump(variance_across_para_time, out_file)
+            # save the with-time data
+            with open("dao_performance_across_time_freq_{}_lr_{}".format(turbulence_freq, lr), 'wb') as out_file:
+                pickle.dump(performance_across_time, out_file)
+            with open("dao_consensus_across_time_freq_{}_lr_{}".format(turbulence_freq, lr), 'wb') as out_file:
+                pickle.dump(superior_performance_across_time, out_file)
+            with open("dao_diversity_across_time_freq_{}_lr_{}".format(turbulence_freq, lr), 'wb') as out_file:
+                pickle.dump(diversity_across_time, out_file)
+            with open("dao_variance_across_time_freq_{}_lr_{}".format(turbulence_freq, lr), 'wb') as out_file:
+                pickle.dump(variance_across_time, out_file)
 
     t1 = time.time()
-    print(time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
-
-
-
-
+    print("Hierarchy: ", time.strftime("%H:%M:%S", time.gmtime(t1 - t0)))
