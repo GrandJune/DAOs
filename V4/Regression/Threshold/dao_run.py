95265ce75418c21872f7c113d36c562db6f5a306
毕夏 <978260577@qq.com>
2024-09-10 14:08:45 +0800

    Add regression analysis

--- V4/Regression/Threshold/dao_run.py
+++ V4/Regression/Threshold/dao_run.py
@@ -14,30 +14,21 @@ import pickle
 import os
 
 
-def func(m=None, n=None, group_size=None, lr=None, asymmetry=None, incentive=None, active_rate=None,
+def func(m=None, n=None, group_size=None, lr=None, threshold_ratio=None, turbulence_freq=None, turbulence_level=None,
          search_loop=None, loop=None, return_dict=None, sema=None):
     np.random.seed(None)
-    mode = 1 # minimal token number
     reality = Reality(m=m)
     dao = DAO(m=m, n=n, reality=reality, lr=lr, group_size=group_size)
-    # pre-assign the token according to the asymmetry degree
-    if asymmetry == 0:
-        for team in dao.teams:
-            for individual in team.individuals:
-                individual.token = 1
-    else:
-        for team in dao.teams:
-            for individual in team.individuals:
-                individual.token = (np.random.pareto(a=asymmetry) + 1) * mode
-
-        # calculate the Gini index of the generated token
-        token_list = []
-        for team in dao.teams:
-            token_list += [individual.token for individual in team.individuals]
     for period in range(search_loop):
-        dao.search(threshold_ratio=0.5, token=True)
-    return_dict[loop] = [dao.performance_across_time, dao.consensus_performance_across_time,
-                         dao.diversity_across_time, dao.variance_across_time]
+        if (period + 1) % turbulence_freq == 0:
+            reality.change(reality_change_rate=turbulence_level)
+            for team in dao.teams:
+                for individual in team.individuals:
+                    individual.payoff = reality.get_payoff(belief=individual.belief)
+        dao.search(threshold_ratio=threshold_ratio, token=False)
+    return_dict[loop] = [dao.performance_across_time[-1], dao.performance_across_time[-2], dao.performance_across_time[-3],
+                         dao.performance_across_time[-4], dao.performance_across_time[-5], threshold_ratio,
+                         turbulence_freq, turbulence_level, lr]
     sema.release()
 
 
@@ -48,7 +39,7 @@ if __name__ == '__main__':
     n = 350
     lr = 0.3
     repetition = 50
-    search_loop = 2000
+    search_loop = 1000
     threshold_ratio_list = np.arange(0.40, 0.71, 0.01)
     group_size = 7  # the smallest group size in Fang's model: 7
 
@@ -58,20 +49,25 @@ if __name__ == '__main__':
     return_dict = manager.dict()
     jobs = []
     for loop in range(repetition):
-        threshold_ratio = np.random.uniform(0.40, 0.71)
+        threshold_ratio = np.random.choice(threshold_ratio_list)
+        lr = np.random.uniform(0, 1)
+        turbulence_freq = np.random.choice([20, 40, 60, 80, 100])
+        turbulence_level = np.random.choice([0.10, 0.12, 0.14, 0.16, 0.18, 0.20])
         sema.acquire()
         p = mp.Process(target=func,
-                       args=(m, n, group_size, lr, threshold_ratio, search_loop, loop, return_dict, sema))
+                       args=(m, n, group_size, lr, threshold_ratio, turbulence_freq, turbulence_level, search_loop, loop, return_dict, sema))
         jobs.append(p)
         p.start()
     for proc in jobs:
         proc.join()
     results = return_dict.values()  # Don't need dict index, since it is repetition.
-    index = 1
-    while os.path.exists(r"dao_data_{0}".format(index)):
-        index += 1
-
-    with open(r"dao_data_{0}".format(index), 'wb') as out_file:
+    # Automatic integration of results
+    time.sleep(np.random.uniform(low=2, high=12))
+    if os.path.exists("dao_data"):
+        with open("dao_data", 'rb') as infile:
+            prior_results = pickle.load(infile)
+            results += prior_results
+    with open("dao_data", 'wb') as out_file:
         pickle.dump(results, out_file)
 
     t1 = time.time()
