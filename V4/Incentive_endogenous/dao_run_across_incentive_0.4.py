111a44fc8df72716647a668386e1babe0f97c0a2
毕夏 <978260577@qq.com>
2024-10-30 21:13:31 +0800

    Add regression analysis

--- V4/Incentive_endogenous/dao_run_across_incentive_0.4.py
+++ V4/Incentive_endogenous/dao_run_across_incentive_0.4.py
@@ -163,4 +163,5 @@ if __name__ == '__main__':
     #     pickle.dump(variance_final_across_incentive_time, out_file)
 
     t1 = time.time()
-    print("active=0.4", time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(t1 - t0)))
+    print(time.strftime("%H:%M:%S", time.gmtime(t1 - t0)))  # Duration
+    print("active=0.4", time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time())))  # Complete time
